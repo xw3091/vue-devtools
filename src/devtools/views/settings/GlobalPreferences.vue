e631c305c3bbd99ad6d6c47fd7fd812faaab323f
xw3091 <wx3091@126.com>
2019-10-09 16:30:29 +0800

    vue-devtools

---
+++ src/devtools/views/settings/GlobalPreferences.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="global-preferences preferences">
+    <VueFormField title="Normalize component names">
+      <VueGroup
+        v-model="$shared.classifyComponents"
+        class="extend"
+      >
+        <VueGroupButton
+          :value="false"
+          label="Original name"
+        />
+        <VueGroupButton
+          :value="true"
+          label="Pascal case"
+        />
+      </VueGroup>
+    </VueFormField>
+
+    <VueFormField title="Theme">
+      <VueGroup
+        v-model="$shared.theme"
+        class="extend"
+      >
+        <VueGroupButton
+          value="auto"
+          label="Auto"
+        />
+        <VueGroupButton
+          value="light"
+          label="Light"
+        />
+        <VueGroupButton
+          value="dark"
+          label="Dark"
+        />
+      </VueGroup>
+    </VueFormField>
+
+    <VueFormField title="Display density">
+      <VueGroup
+        v-model="$shared.displayDensity"
+        class="extend"
+      >
+        <VueGroupButton
+          value="auto"
+          label="Auto"
+        />
+        <VueGroupButton
+          value="low"
+          label="Low"
+        />
+        <VueGroupButton
+          value="high"
+          label="High"
+        />
+      </VueGroup>
+    </VueFormField>
+
+    <VueFormField
+      title="Editable props"
+    >
+      <VueSwitch v-model="$shared.editableProps">
+        Enable
+      </VueSwitch>
+      <template #subtitle>
+        <VueIcon
+          icon="warning"
+          class="medium"
+        />
+        May print warnings in the console
+      </template>
+    </VueFormField>
+
+    <VueFormField title="Detected Vue message">
+      <VueSwitch v-model="$shared.logDetected">
+        Display in browser console
+      </VueSwitch>
+    </VueFormField>
+
+    <VueFormField
+      title="Autoload Vuex state"
+    >
+      <VueSwitch v-model="$shared.vuexAutoload">
+        Enable
+      </VueSwitch>
+      <template #subtitle>
+        <VueIcon
+          icon="warning"
+          class="medium"
+        />
+        May impact performance or cause crashes
+      </template>
+    </VueFormField>
+
+    <VueFormField
+      title="Vuex Legacy"
+    >
+      <VueSwitch v-model="$shared.vuex1">
+        Enable compatibility mode
+      </VueSwitch>
+      <template #subtitle>
+        <VueIcon
+          icon="warning"
+          class="medium"
+        />
+        If you use Vuex 1.x, enable this option
+      </template>
+    </VueFormField>
+  </div>
+</template>
---
+++ src/devtools/views/settings/SettingsTab.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="settings grid">
+    <GlobalPreferences />
+  </div>
+</template>
+
+<script>
+import GlobalPreferences from './GlobalPreferences.vue'
+import { mapState } from 'vuex'
+
+export default {
+  components: { GlobalPreferences },
+
+  computed: mapState('events', [
+    'enabled'
+  ])
+}
+</script>
+
+<style lang="stylus" scoped>
+.settings
+  overflow auto
+  >>> .preferences
+    display flex
+    flex-wrap wrap
+    padding 12px 4px
+
+    > *
+      flex-basis 300px
+      margin 16px 24px
+
+    .vue-ui-form-field
+      > .wrapper > .content
+       min-height 32px
+       justify-content center
+</style>
